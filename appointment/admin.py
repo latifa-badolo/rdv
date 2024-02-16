107fb7ca171c5cf43b2fed0b577b0652b057d741
Ibrahim Bako <ibrahim.bako@kreezus.com>
2024-02-16 00:33:19 +0000

    ajout de quelque example de template et configuration de l'admin

--- appointment/admin.py
+++ appointment/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import ( Appointment, Attendee, Availability, AvailabilityInterval, Calendar)
 
-# Register your models here.
+
+class AvailabilityIntervalInlineInline(admin.TabularInline):
+    model = AvailabilityInterval
+
+@admin.register(Calendar)
+class CalendarAdmin(admin.ModelAdmin):
+    list_display = ["user", "enppointment_duration_in_minutes", "active", "updated_at"]
+
+@admin.register(Availability)
+class AvailabilityAdmin(admin.ModelAdmin):
+    list_display = ["calendar", "day_of_week", "updated_at"]
+    inlines = [AvailabilityIntervalInlineInline]
+
+@admin.register(AvailabilityInterval)
+class AvailabilityIntervalAdmin(admin.ModelAdmin):
+    list_display = ["start_time", "end_time",]
+
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ["date", "start_time", "end_time", "service", "status", "updated_at"]
+
+@admin.register(Attendee)
+class AttendeeAdmin(admin.ModelAdmin):
+    list_display = ["appointment", "first_name", "last_name", "email", "phone_number", "updated_at"]
--- appointment/urls.py
+++ appointment/urls.py
@@ -1,3 +1,9 @@
 from django.urls import path
 
-urlpatterns = []
+from .views import (
+    see_calendar,
+)
+
+urlpatterns = [
+    path("see_calendar/<int:service_provider_id>", see_calendar, name="see_calendar")
+]
